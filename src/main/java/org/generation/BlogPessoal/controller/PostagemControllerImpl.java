9bb182b6e1685308d97546838e1e1ae850e192e7
Luana Bezana <luana.bezana@hotmail.com>
2021-05-02 08:45:01 -0300

    Subindo blog generation.

---
+++ src/main/java/org/generation/BlogPessoal/controller/PostagemControllerImpl.java
@@ -0,0 +1,26 @@
+package org.generation.BlogPessoal.controller;
+
+import java.util.List;
+
+import org.generation.BlogPessoal.model.Postagem;
+import org.generation.BlogPessoal.repository.PostagemRepositoryCustom;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/postagens")
+@CrossOrigin("*")
+public class PostagemControllerImpl {
+
+	@Autowired
+	private PostagemRepositoryCustom repository;
+	
+	@GetMapping
+	public ResponseEntity<List<Postagem>> GetAll(){
+		return ResponseEntity.ok(repository.findAll());
+	}
+}
